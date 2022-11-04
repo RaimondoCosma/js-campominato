bc2a080b82570b995bce0f9a53deff09fe9b1feb
RaimondoCosma <raimondo.cosma@gmail.com>
2022-11-04 22:02:31 +0100

    Aggiunge evento blocco e colora tutte le bombe

--- js/main.js
+++ js/main.js
@@ -25,41 +25,47 @@ function boxCreation (howManyColumn) {
     }
     console.log(bombNumbers);
     // Inizializzo un ciclo for per creare i miei box dinamicamente dentro il box__container
+    let gameOver = false;
     for ( let i = 1; i <= howManyBox; i++ ){
         let boxItem = document.createElement("div");
         boxItem.innerHTML = i;
         boxItem.classList.add('box__item',`box__item${howManyColumn}`);
         boxContainer.append(boxItem);
         // Aggiungo evento click al box della cella
-        boxItem.addEventListener("click", function abc(e){
+        boxItem.addEventListener("click", function(){
+            if ( gameOver ){
+                return;
+            }
             let boxBomb = bombNumbers.includes(Number(boxItem.innerHTML));
             const reset = document.getElementById('reset');
             reset.addEventListener('click', function(){
                 window.location.reload();
             });
-            const advice = document.querySelector('#advice');
             if ( boxBomb ){
-                boxItem.classList.add("bg-change-error");                
-                loseBanner.classList.add('show');
-                boxItem.removeEventListener('click', abc);
                 advice.innerHTML = `<span style="font-size:24px; color:red;">Gioco finito, premi il tasto reset se vuoi ricominciare.</span>`;
+                loseBanner.classList.add('show');
                 reset.classList.add('show');
-                return;
+                for ( let i = 0; i < bombNumbers.length; i++ ){
+                    document.querySelector(`.box__item:nth-child(${bombNumbers[i]}`).classList.add('bg-change-error');
+                }
+                gameOver = true;
             } else {
                 if ( !this.classList.contains("bg-change") ){
                     this.classList.add("bg-change");
                     score++;
+                    advice.innerHTML = `<span style="font-size:24px;">Il tuo punteggio attuale è di: ${score} punti</span>`;
                 } else {
                     score = score;
                 }
-                if ( score === howManyBox - 16 ){
-                    winBanner.classList.add('show');
-                }
-                totalWinScore.innerHTML = `<span style="font-size: 30px; color: red; font-weight: bold">${score} su ${score}</score>`;
-                totalScore.innerHTML = `<span style="font-size: 30px; color: red; font-weight: bold">${score}</score>`;
-                advice.innerHTML = `<span style="font-size:24px; color:red;">Gioco finito, premi il tasto reset se vuoi ricominciare.</span>`;
             }
-            advice.innerHTML = `<span style="font-size:24px;">Il tuo punteggio attuale è di: ${score} punti</span>`;
+            if ( score === howManyBox - 16 ){
+                winBanner.classList.add('show');
+                advice.innerHTML = `<span style="font-size:24px; color:red;">Gioco finito, premi il tasto reset se vuoi ricominciare.</span>`;
+                reset.classList.add('show');
+                gameOver = true;
+            }
+            totalWinScore.innerHTML = `<span style="font-size: 30px; color: red; font-weight: bold">${score} su ${score}</score>`;
+            totalScore.innerHTML = `<span style="font-size: 30px; color: red; font-weight: bold">${score}</score>`;
             console.log(`Hai cliccato la casella numero: ${this.innerHTML}`);
         })
     }
@@ -69,6 +75,7 @@ function boxCreation (howManyColumn) {
 -----------------------*/
 // Dichiaro la variabile relativa al box__container
 const boxContainer = document.getElementById('box__container');
+const advice = document.querySelector('#advice');
 
 // Dichiaro la variabile relativa al bottone play
 const play = document.getElementById('play__btn');
